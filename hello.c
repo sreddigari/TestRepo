cd80ac27b9c8f4cb744df0af842f53ad3d7ce4b4
Sudhakar Reddy <s.reddigari@f5.com>
2020-08-12 01:23:03 -0700

    changing the logic

--- hello.c
+++ hello.c
@@ -1,4 +1,18 @@
-main () 
-{
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+int add_numbers(int a, int b) {
+    return a+b;
+}
+
+int main() {
+    int num1=5, num2=6;
+    int sum;
+
+    sum = add_numbers(num1,num2);
+    printf("The sum is %d",sum);
+
+    return 0;
 }
